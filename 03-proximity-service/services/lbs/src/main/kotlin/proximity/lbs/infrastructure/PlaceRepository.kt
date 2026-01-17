3acadf4571a84f74a714ded082d25b44f12e4944
nmin11 <nmin1124@gmail.com>
2026-01-17 16:40:27 +0900

    fix: naver map 연동 및 Spring 에러 해결

--- 03-proximity-service/services/lbs/src/main/kotlin/proximity/lbs/infrastructure/PlaceRepository.kt
+++ 03-proximity-service/services/lbs/src/main/kotlin/proximity/lbs/infrastructure/PlaceRepository.kt
@@ -9,20 +9,20 @@ import proximity.lbs.domain.Place
 interface PlaceRepository : JpaRepository<Place, Long> {
 
     @Query("""
-        SELECT p FROM Place p
+        SELECT * FROM places p
         WHERE ST_DWithin(p.location, :point, :radius) = true
         ORDER BY ST_Distance(p.location, :point)
-    """)
+    """, nativeQuery = true)
     fun findNearby(
         @Param("point") point: Point,
         @Param("radius") radius: Double
     ): List<Place>
 
     @Query("""
-        SELECT p FROM Place p
+        SELECT * FROM places p
         ORDER BY ST_Distance(p.location, :point)
         LIMIT :limit
-    """)
+    """, nativeQuery = true)
     fun findNearest(
         @Param("point") point: Point,
         @Param("limit") limit: Int
